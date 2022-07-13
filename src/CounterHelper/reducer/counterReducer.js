ee7aad1e9b4bf053c6fe243980dab3102047fdf9
jobayercse71 <jobayercse71.com>
2022-07-13 22:34:18 +0600

    action added

--- src/CounterHelper/reducer/counterReducer.js
+++ src/CounterHelper/reducer/counterReducer.js
@@ -1,3 +1,4 @@
+import { DECREMENT, INCREMENT, RESET } from "../constants/constants";
 
 const initialState ={
     count:0
@@ -6,4 +7,26 @@ const initialState ={
 
 const counterReducer =(state=initialState,action)=>{
 
+    switch (action.type) {
+        case INCREMENT:
+            
+            return{
+                ...state, count:state.count+1
+            }
+        case DECREMENT:
+            
+            return{
+                ...state, count:state.count-1
+            }
+        case RESET:
+            
+            return{
+                ...state, count:0
+            }
+    
+        default:
+          return state;
+    }
+
 }
+export default counterReducer;

542415de5a746b4a033dcd91ec8b071ba44d99ae
jobayercse71 <jobayercse71.com>
2022-07-13 23:11:45 +0600

    dispatch added in counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
+    const count = useSelector(state => state.count);
+    const dispatch = useDispatch();
+    const handleIncrement = () => {
+        dispatch({ type: 'INCREMENT' })
+    }
+    const handleDecrement = () => {
+        dispatch({ type: 'DECREMENT' })
+    }
+    const handleReset = () => {
+        dispatch({ type: 'RESET' })
+    }
     return (
         <div>
-            <h1>count: </h1>
-            <button>increment</button>
+            <h1>count:{count}</h1>
+            <button onClick={handleIncrement}>increment</button>
+            <button onClick={handleDecrement}>decrement</button>
+            <button onClick={handleReset}>reset</button>
         </div>
     );
 };
